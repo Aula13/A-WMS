640d2c28a1cfa16d5ca4f9d74d0a91a5be5e1d49
st.pessina <st.pessina@gmail.com>
2015-05-01 16:18:44 +0200

    initial commit

---
+++ src/org/wms/main/WMS.java
@@ -0,0 +1,10 @@
+package org.wms.main;
+
+public class WMS {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ test/org/wms/main/WMSTest.java
@@ -0,0 +1,14 @@
+package org.wms.main;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class WMSTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
