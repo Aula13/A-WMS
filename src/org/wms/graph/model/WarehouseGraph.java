31e21035e45ecc7693787b486888e1e98419d8c4
danieleciriello <ciriello.daniele@gmail.com>
2015-06-06 11:31:55 +0200

    continues graph structure

--- src/org/wms/graph/model/WarehouseGraph.java
+++ src/org/wms/graph/model/WarehouseGraph.java
@@ -3,35 +3,37 @@
  */
 package org.wms.graph.model;
 
-import java.util.Collection;
+import org.apache.commons.collections15.Factory;
+import org.wms.model.warehouse.Warehouse;
 
 import edu.uci.ics.jung.graph.Graph;
-import edu.uci.ics.jung.graph.util.EdgeType;
-import edu.uci.ics.jung.graph.util.Pair;
-
+import edu.uci.ics.jung.graph.UndirectedGraph;
+import edu.uci.ics.jung.graph.UndirectedSparseGraph;
 /**
  * @author Stefano Pessina, Daniele Ciriello
  *
  * @param <E>
  * @param <V>
  */
-public class WarehouseGraph extends DelegatingGraph<V, E> implements Graph<V, E> {
+public class WarehouseGraph extends DelegatingGraph<WarehouseVertex, WarehouseEdge> implements Graph<WarehouseVertex, WarehouseEdge> {
 
 	public WarehouseGraph(Graph<WarehouseVertex, WarehouseEdge> graph) {
 		super(graph);
 		// TODO Auto-generated constructor stub
 	}
 	
-	private static UndirectedGraph<Integer, Integer> generateGraph(int numberOfVertices) {
-		CompleteGraphGenerator<Integer, Integer> generator = new CompleteGraphGenerator<Integer, Integer>(
-				new Factory<UndirectedGraph<Integer, Integer>>() {
-
-					@Override
-					public UndirectedGraph<Integer, Integer> create() {
-						return new UndirectedSparseGraph<Integer, Integer>();
-					}
-				}, new IntegerFactory(), new IntegerFactory(), numberOfVertices);
-		return (UndirectedGraph<Integer, Integer>) generator.create();
+	private static Graph<WarehouseVertex, WarehouseEdge> generateGraph(Warehouse warehouse) {
+//		CompleteGraphGenerator<Integer, Integer> generator = new CompleteGraphGenerator<Integer, Integer>(
+//				new Factory<UndirectedGraph<Integer, Integer>>() {
+//
+//					@Override
+//					public UndirectedGraph<Integer, Integer> create() {
+//						return new UndirectedSparseGraph<Integer, Integer>();
+//					}
+//				}, new IntegerFactory(), new IntegerFactory(), numberOfVertices);
+//		return (Graph<WarehouseVertex, WarehouseEdge>) generator.create();
+		
+	
 	}
 
 
