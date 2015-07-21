d74bdd13a6f2f2e839e3eb73add438bf673f28ca
st.pessina <st.pessina@gmail.com>
2015-07-21 23:14:38 +0200

    Unit test for WarehouseLineView

--- test/org/wms/view/warehouse/WarehouseLineViewUnitTest.java
+++ test/org/wms/view/warehouse/WarehouseLineViewUnitTest.java
@@ -1,14 +1,43 @@
 package org.wms.view.warehouse;
 
-import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.wms.model.warehouse.WarehouseLine;
+import org.wms.model.warehouse.WarehouseShelf;
+import org.wms.test.TestUtils;
 
 public class WarehouseLineViewUnitTest {
 
+	private static WarehouseLine mockWarehouseLine;
+	
+	private static WarehouseShelf mockWarehouseShelf;
+	
+	@BeforeClass
+	public static void setupBeforeClass() {
+		TestUtils.initGUIFactories();
+		
+		mockWarehouseLine = mock(WarehouseLine.class);
+		mockWarehouseShelf = mock(WarehouseShelf.class);
+		
+		when(mockWarehouseLine.getPublicId()).thenReturn("A");
+		
+		List<WarehouseShelf> shelfs = new ArrayList<WarehouseShelf>();
+		shelfs.add(mockWarehouseShelf);
+		
+		when(mockWarehouseLine.getUnmodifiableListShelfs()).thenReturn(shelfs);
+		
+		when(mockWarehouseShelf.getPublicId()).thenReturn("A/1");
+	}
+	
 	@Test
 	public void testWarehouseLineView() {
-		//fail("Not yet implemented");
+		WarehouseLineView view = new WarehouseLineView(mockWarehouseLine);
 	}
 
 }
